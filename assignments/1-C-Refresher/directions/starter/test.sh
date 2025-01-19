bcd34a15bba58c0fa808c3c32dcefe7c1757c0c7
Alex651907 <alex651907@gmail.com>
2025-01-19 14:22:24 -0500

    Updated test cases to better align with assignment instructions

--- assignments/1-C-Refresher/directions/starter/test.sh
+++ assignments/1-C-Refresher/directions/starter/test.sh
@@ -35,7 +35,8 @@ Buffer:  [The strange spaces should be removed from this....]" ]
 @test "reverse" {
     run ./stringfun -r "Reversed sentences look very weird"
     [ "$status" -eq 0 ]
-    [ "$output" = "Buffer:  [driew yrev kool secnetnes desreveR................]" ]
+    [ "$output" = "Reversed String: driew yrev kool secnetnes desreveR
+Buffer:  [................driew yrev kool secnetnes desreveR]" ]
 }
 
 @test "print words" {
@@ -43,14 +44,14 @@ Buffer:  [The strange spaces should be removed from this....]" ]
     [ "$status" -eq 0 ]
     [ "$output" = "Word Print
 ----------
-1. Lets(4)
-2. get(3)
-3. a(1)
-4. lot(3)
-5. of(2)
-6. words(5)
-7. to(2)
-8. test(4)
+1. Lets (4)
+2. get (3)
+3. a (1)
+4. lot (3)
+5. of (2)
+6. words (5)
+7. to (2)
+8. test (4)
 
 Number of words returned: 8
 Buffer:  [Lets get a lot of words to test...................]" ]
@@ -59,7 +60,8 @@ Buffer:  [Lets get a lot of words to test...................]" ]
 @test "check max length" {
     run ./stringfun -r "This is the maximum length string that should work"
     [ "$status" -eq 0 ]
-    [ "$output" = "Buffer:  [krow dluohs taht gnirts htgnel mumixam eht si sihT]" ]
+    [ "$output" = "Reversed String: krow dluohs taht gnirts htgnel mumixam eht si sihT
+Buffer:  [krow dluohs taht gnirts htgnel mumixam eht si sihT]" ]
 }
 
 @test "check over max length" {
